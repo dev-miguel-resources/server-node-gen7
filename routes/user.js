c051045f46421c01a0e0e2045275119eb897ebfa
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-11-16 23:45:43 -0300

    example endpoint, model user, server routes and firebase initialize

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+// routes
+router.get("/user", (req, res, next) => {
+  res.json({
+    data: "hey you hit user API endpoint",
+  });
+});
+
+// router.post("/product", authCheck, adminCheck, create);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const connectDB = require("./database");
 const cors = require("cors");
 const morgan = require("morgan");
 const { readdirSync } = require("fs");
-//const bodyParser = require("body-parser"); deprecated node +12
+//const bodyParser = require("body-parser"); deprecated node >12
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
+//const userRoutes = require("./routes/user");
 require("dotenv").config();
 
 // app-server
@@ -20,7 +21,14 @@ app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// routes middlewares-fs
+// routes: option 1
+/*app.use("/api", userRoutes);
+app.use("/api", authRoutes);
+app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);*/
+
+// routes middlewares-fs: option 2 (optime)
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
