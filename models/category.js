524babcfd47ed9247affec757dde96d2bd01bbc8
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-11-23 20:15:49 -0300

    category, sub and product models

---
+++ models/category.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Name is required",
+      minlength: [2, "Too short"],
+      maxlength: [32, "Too long"],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    status: {
+      type: String,
+      default: "Active",
+      enum: ["Active", "Inactive"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
---
+++ models/sub.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { ObjectId } = require("mongoose");
+
+const subSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Name is required",
+      minlength: [2, "Too short"],
+      maxlength: [32, "Too long"],
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    parent: { type: ObjectId, ref: "Category", required: true },
+    status: {
+      type: String,
+      default: "Active",
+      enum: ["Active", "Inactive"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Sub", subSchema);
