51347e1082d8b7c425ed12d91f48ba9dba1782f6
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-11-23 23:04:26 -0300

    completed models

---
+++ models/coupon.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const couponSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+      uppercase: true,
+      required: "Name is required",
+      minlength: [6, "Too short"],
+      maxlength: [12, "Too long"],
+    },
+    expiry: {
+      type: Date,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "Active",
+      enum: ["Active", "Inactive"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Coupon", couponSchema);
