24ca35efeb254e2369cffc5252de28367931f31a
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-08 09:43:28 -0300

    working in the category documentation

--- routes/category.js
+++ routes/category.js
@@ -5,6 +5,43 @@ const router = express.Router();
 const { create } = require("../controllers/category");
 
 // routes-endpoints
+
+// ROUTES - SWAGGER
+
 router.post("/category", create);
 
 module.exports = router;
+
+
+// SCHEMAS - SWAGGER
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         name:
+ *            type: string
+ *            trim: true
+ *            minlength: 2
+ *            maxlength: 32
+ *         slug:
+ *            type: string
+ *            unique: true
+ *            lowercase: true
+ *            index: true
+ *         status:
+ *            type: string
+ *            default: "Active"
+ *            enum:
+ *            - "Active"
+ *            - "Inactive"
+ *       example:
+ *         name: Fashion
+ *         slug: fashion
+ *         status: Active      
+ */
--- routes/product.js
+++ routes/product.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const { create, productsCount, listAll, remove, remove2, read, update } = require("../controllers/product");
 
 // routes-endpoints product
+// ROUTES - SWAGGER
+
+
 router.post("/product", create);
 
 /**
@@ -20,12 +23,61 @@ router.post("/product", create);
  */
 router.get("/products/total", productsCount);
 
-
+/**
+ * @swagger
+ * /products/{count}:
+ *   get:
+ *     tags:
+ *       - name: "Product"
+ *     summary: "All products active by count"
+ *     parameters:
+ *       - name: "count"
+ *         in: "path"
+ *         description: "count product search"
+ *         required: true
+ *         type: "integer"
+ *         format: "int64"
+ *     responses:
+ *       200: 
+ *          description: ok   
+ */
 router.get("/products/:count", listAll); // products/50
+
 router.patch("/product/:slug", remove); // soft-delete
 router.delete("/product/:slug", remove2); // no soft-delete
 router.get("/product/:slug", read);
 router.put("/product/:slug", update);
 
 module.exports = router;
+
+// SCHEMAS - SWAGGER
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *         - price
+ *       properties:
+ *         title:
+ *            type: string
+ *            trim: true
+ *            maxlength: 32
+ *            text: true
+ *         description:
+ *            type: string
+ *            maxlength: 2000
+ *            text: true
+ *         price:
+ *            type: "integer"
+ *            trim: true
+ *            maxlength: 32
+ */
+
+
+
 
